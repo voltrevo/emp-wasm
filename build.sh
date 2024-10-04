cc52638f06600d7c92afe0fe598c612da946a3bc
Andrew Morris <voltrevo@gmail.com>
2024-10-04 17:20:54 +1000

    got it compiling

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clang++ \
+    -std=c++17 \
+    programs/2pc.cpp \
+    -I src/ \
+    -I $(brew --prefix openssl)/include \
+    -L $(brew --prefix openssl)/lib \
+    -lcrypto \
+    -lssl \
+    -o 2pc
