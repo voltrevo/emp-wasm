69eec6b159eab4687dcc71aab1ae70f77a898b5c
Andrew Morris <voltrevo@gmail.com>
2024-10-15 10:05:08 +1100

    Export BufferQueue

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,4 @@
 export { default as secure2PC } from "./secure2PC";
 export { default as BufferedIO } from "./BufferedIO";
+export { default as BufferQueue } from "./BufferQueue";
 export { type IO } from "./types";
