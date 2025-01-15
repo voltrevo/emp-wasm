effeda96aa6304d46666cffdb83188a98df0a4f5
Andrew Morris <voltrevo@gmail.com>
2025-01-15 14:44:43 +1100

    got mpc test compiling

--- scripts/build_mpc_test.sh
+++ scripts/build_mpc_test.sh
@@ -5,11 +5,10 @@ clang++ \
     -std=c++17 \
     programs/test_mpc.cpp \
     -I src/cpp/mpc \
-    -I $(brew --prefix mbedtls)/include \
-    -L $(brew --prefix mbedtls)/lib \
-    -lmbedtls \
-    -lmbedcrypto \
-    -lmbedx509 \
+    -I $(brew --prefix openssl)/include \
+    -L $(brew --prefix openssl)/lib \
+    -lcrypto \
+    -lssl \
     -o build/mpc
 
 echo "Build successful, use ./scripts/local_test.sh to run the program."
