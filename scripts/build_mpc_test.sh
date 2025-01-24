1ed3c0aebeec37346465aa11eda75aefb8655dbb
Andrew Morris <voltrevo@gmail.com>
2025-01-24 12:33:50 +1100

    consolidate 2pc and mpc overlapping deps

--- scripts/build_mpc_test.sh
+++ scripts/build_mpc_test.sh
@@ -6,7 +6,7 @@ clang++ \
     -O3 \
     -std=c++17 \
     programs/test_mpc.cpp \
-    -I src/cpp/mpc \
+    -I src/cpp \
     -I $(brew --prefix mbedtls)/include \
     -L $(brew --prefix mbedtls)/lib \
     -lmbedtls \
--- src/cpp/mpc/emp-ot/emp-ot.h
+++
@@ -1,3 +0,0 @@
-#include "emp-ot/ot.h"
-#include "emp-ot/co.h"
-#include "emp-ot/iknp.h"
